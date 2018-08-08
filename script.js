73542a490dc012f64e8e5dfa348cb7c5d0e89861
Geoff <geoff.register@gmail.com>
2018-08-08 18:15:58 -0400

    First add for my own work

---
+++ script.js
@@ -0,0 +1,97 @@
+var one = (document.querySelector(".one"))
+one.addEventListener('click', function () {
+    console.log(1)
+})
+
+var two = (document.querySelector(".two"))
+two.addEventListener('click', function () {
+    console.log(2)
+})
+
+var three = (document.querySelector(".three"))
+three.addEventListener('click', function () {
+    console.log(3)
+})
+
+var four = (document.querySelector(".four"))
+four.addEventListener('click', function () {
+    console.log(4)
+})
+
+var five = (document.querySelector(".five"))
+five.addEventListener('click', function () {
+    console.log(5)
+})
+
+var six = (document.querySelector(".six"))
+six.addEventListener('click', function () {
+    console.log(6)
+})
+
+var seven = (document.querySelector(".seven"))
+seven.addEventListener('click', function () {
+    console.log(7)
+})
+
+var eight = (document.querySelector(".eight"))
+eight.addEventListener('click', function () {
+    console.log(8)
+})
+
+var nine = (document.querySelector(".nine"))
+nine.addEventListener('click', function () {
+    console.log(9)
+})
+
+var zero = (document.querySelector(".zero"))
+zero.addEventListener('click', function () {
+    console.log(0)
+})
+
+var decimal = (document.querySelector(".decimal"))
+decimal.addEventListener('click', function () {
+    console.log('.')
+})
+
+var addd = (document.querySelector(".addd"))
+addd.addEventListener('click', function () {
+    console.log('+')
+})
+
+var subtract = (document.querySelector(".subtract"))
+subtract.addEventListener('click', function () {
+    console.log('-')
+})
+
+var multiply = (document.querySelector(".multiply"))
+multiply.addEventListener('click', function () {
+    console.log('*')
+})
+
+var divide = (document.querySelector(".divide"))
+divide.addEventListener('click', function () {
+    console.log('/')
+})
+
+var calculate = (document.querySelector(".calculate"))
+calculate.addEventListener('click', function () {
+    console.log('=')
+})
+
+var clear = (document.querySelector(".clear"))
+clear.addEventListener('click', function () {
+    console.log('\
+    (clear')
+})
+
+var screen = (document.querySelector('.screen'))
+screen.innerHTML = '<div>'
+
+
+
+
+
+
+
+
+
