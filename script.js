b78f1e11263e19bb7832a048d35aa9aafcdd6a12
Geoff <geoff.register@gmail.com>
2018-08-09 13:34:44 -0400

    after tinkering in group

--- script.js
+++ script.js
@@ -2,88 +2,88 @@ var screenWrite = (document.querySelector('.empty_div'))
 
 var one = (document.querySelector(".one"))
 one.addEventListener('click', function () {
-    screenWrite.innerHTML = 1
+    screenWrite.innerHTML = screenWrite.innerHTML = screenWrite.innerHTML + ('1')
 })
 
 var two = (document.querySelector(".two"))
+
 two.addEventListener('click', function () {
-    screenWrite.innerHTML = 2
+    screenWrite.innerHTML = screenWrite.innerHTML + ('2')
 })
 
 var three = (document.querySelector(".three"))
 three.addEventListener('click', function () {
-    console.log(3)
+    screenWrite.innerHTML = screenWrite.innerHTML + ('3')
 })
 
 var four = (document.querySelector(".four"))
 four.addEventListener('click', function () {
-    console.log(4)
+    screenWrite.innerHTML = screenWrite.innerHTML + ('4')
 })
 
 var five = (document.querySelector(".five"))
 five.addEventListener('click', function () {
-    console.log(5)
+    screenWrite.innerHTML = screenWrite.innerHTML + ('5')
 })
 
 var six = (document.querySelector(".six"))
 six.addEventListener('click', function () {
-    console.log(6)
+    screenWrite.innerHTML = screenWrite.innerHTML + ('6')
 })
 
 var seven = (document.querySelector(".seven"))
 seven.addEventListener('click', function () {
-    console.log(7)
+    screenWrite.innerHTML = screenWrite.innerHTML + ('7')
 })
 
 var eight = (document.querySelector(".eight"))
 eight.addEventListener('click', function () {
-    console.log(8)
+    screenWrite.innerHTML = screenWrite.innerHTML + ('8')
 })
 
 var nine = (document.querySelector(".nine"))
 nine.addEventListener('click', function () {
-    console.log(9)
+    screenWrite.innerHTML = screenWrite.innerHTML + ('9')
 })
 
 var zero = (document.querySelector(".zero"))
 zero.addEventListener('click', function () {
-    console.log(0)
+    screenWrite.innerHTML = screenWrite.innerHTML + ('0')
 })
 
 var decimal = (document.querySelector(".decimal"))
 decimal.addEventListener('click', function () {
-    console.log('.')
+    screenWrite.innerHTML = screenWrite.innerHTML + ('.')
 })
 
 var addd = (document.querySelector(".addd"))
 addd.addEventListener('click', function () {
-    console.log('+')
+    screenWrite.innerHTML = screenWrite.innerHTML + ('+')
 })
 
 var subtract = (document.querySelector(".subtract"))
 subtract.addEventListener('click', function () {
-    console.log('-')
+    screenWrite.innerHTML = screenWrite.innerHTML + ('-')
 })
 
 var multiply = (document.querySelector(".multiply"))
 multiply.addEventListener('click', function () {
-    console.log('*')
+    screenWrite.innerHTML = screenWrite.innerHTML + ('*')
 })
 
 var divide = (document.querySelector(".divide"))
 divide.addEventListener('click', function () {
-    console.log('/')
+    screenWrite.innerHTML = screenWrite.innerHTML + ('/')
 })
 
 var calculate = (document.querySelector(".calculate"))
 calculate.addEventListener('click', function () {
-    console.log('=')
+    screenWrite.innerHTML = screenWrite.innerHTML + ('=')
 })
 
 var clear = (document.querySelector(".clear"))
 clear.addEventListener('click', function () {
-    console.log('\
-    (clear')
+    screenWrite.innerHTML = ('')
 })
 
 // var screen = (document.querySelector('.screen'))
